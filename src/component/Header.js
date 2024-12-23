1331af939280d7485b7f01982a6b6dd235554ae6
Hitlesh Kumar <hitleshkumar23@navgurukul.org>
2024-12-23 17:44:29 +0530

    Worked on HomePage and Header

--- src/component/Header.js
+++ src/component/Header.js
@@ -4,8 +4,8 @@ import "./Header.css";
 function Header() {
   return (
     <nav className="header">
-      <div className="logo">Dantewada Tourism</div>
-      <ul className="nav-links">
+<div className="logo">Tourist-Buddy</div>
+            <ul className="nav-links">
         <li><a href="#home">Home</a></li>
         <li><a href="#homestays">Homestays</a></li>
         <li><a href="#contact">Contact</a></li>
